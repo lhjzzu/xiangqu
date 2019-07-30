571634719043bf0a6c68d857991d7a9004ef51ef
刘华健 <liuhuajian@liuhuajianMacBookPro.local>
2019-07-30 12:34:31 +0800

    #Upate 添加图片

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import '../network/network.dart';
 import '../model/theme_model.dart';
+import 'dart:ui';
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+
+
+   HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -7,24 +7,24 @@ import './shopping_page.dart';
 import './mine_page.dart';
 
 class IndexPath extends StatelessWidget {
-  final items = [
-    BottomNavigationBarItem(
-      icon: Icon(Icons.home),
-      title: Text("专题"),
-    ),
-    BottomNavigationBarItem(
-      icon:  Icon(Icons.home),
-      title: Text("设计师"),
-    ),
-    BottomNavigationBarItem(
-      icon: Icon(Icons.home),
-      title: Text("逛"),
-    ),
-    BottomNavigationBarItem(
-      icon: Icon(Icons.home),
-      title: Text("我的"),
-    ),
-  ];
+  // final items = [
+  //   BottomNavigationBarItem(
+  //     icon: Image.asset("images/2.0x/icn_guang_highlight.png", fit: BoxFit.cover, width: 25,height: 25,),
+  //     title: Text("专题"),
+  //   ),
+  //   BottomNavigationBarItem(
+  //     icon:  Icon(Icons.home),
+  //     title: Text("设计师"),
+  //   ),
+  //   BottomNavigationBarItem(
+  //     icon: Icon(Icons.home),
+  //     title: Text("逛"),
+  //   ),
+  //   BottomNavigationBarItem(
+  //     icon: Icon(Icons.home),
+  //     title: Text("我的"),
+  //   ),
+  // ];
 
   final pages = [
     HomePage(),
@@ -34,8 +34,43 @@ class IndexPath extends StatelessWidget {
   ];
   IndexPath({Key key}) : super(key: key);
 
+  BottomNavigationBarItem _bottomItem(String title, int index ,int currentIndex) {
+    var normalImageName = "";
+    var selectedImageName = "";
+    if (index == 0) {
+      normalImageName = "images/icn_zhuanti.png";
+      selectedImageName = "images/icn_zhuanti_highlight.png";
+    } else if (index == 1) {
+      normalImageName = "images/icn_designer.png";
+      selectedImageName = "images/icn_designer_highlight.png";
+    } else if (index == 2) {
+      normalImageName = "images/icn_guang.png";
+      selectedImageName = "images/icn_guang_highlight.png";
+    } else if (index == 3) {
+      normalImageName = "images/icn_mycenter.png";
+      selectedImageName = "images/icn_mycenter_highlight.png";
+    } 
+    return BottomNavigationBarItem(
+                icon: Container(
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image:  AssetImage(currentIndex != index ? normalImageName:selectedImageName),
+                      )
+                    ),
+                  ),
+                  width: 25,
+                  height: 25,
+                ),
+                title: Text(title),
+              );
+  }
+
+
   @override
   Widget build(BuildContext context) {
+
+    
     return Provide<IndexProvide>(
       builder: (BuildContext context, Widget child, IndexProvide indexProvide) {
         return Scaffold(
@@ -45,7 +80,12 @@ class IndexPath extends StatelessWidget {
           ),
           bottomNavigationBar: BottomNavigationBar(
             type: BottomNavigationBarType.fixed,
-            items: items,
+            items: [
+              _bottomItem("专题", 0,indexProvide.currentIndex),
+              _bottomItem("设计师", 1,indexProvide.currentIndex),
+              _bottomItem("逛", 2,indexProvide.currentIndex),
+              _bottomItem("我的", 3,indexProvide.currentIndex),              
+            ],
             selectedItemColor: Color.fromARGB(255, 212, 183, 104),
             unselectedItemColor: Colors.black,
             currentIndex: indexProvide.currentIndex,
